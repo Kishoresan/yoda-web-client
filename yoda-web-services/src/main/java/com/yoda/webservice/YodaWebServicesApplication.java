421b3df58b0ac351826c1bd5297873005dfad516
sumeet89 <sumeetrana@sumeets-macbook-air.home>
2019-03-24 13:20:23 +0100

    Committing empty project for yoda web-app web services

---
+++ yoda-web-services/src/main/java/com/yoda/webservice/YodaWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.yoda.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YodaWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YodaWebServicesApplication.class, args);
+	}
+
+}
