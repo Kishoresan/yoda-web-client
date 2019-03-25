6c58b22a52c503323cbc2ef873657ced26358d4e
sumeet89 <sumeetrana@192.168.0.17>
2019-03-25 12:30:25 +0100

    Adding new Spring Boot dependencies to pom

--- yoda-web-services/src/main/java/com/yoda/webservice/YodaWebServicesApplication.java
+++ yoda-web-services/src/main/java/com/yoda/webservice/YodaWebServicesApplication.java
@@ -3,7 +3,7 @@ package com.yoda.webservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.yoda.webservice")
 public class YodaWebServicesApplication {
 
 	public static void main(String[] args) {
