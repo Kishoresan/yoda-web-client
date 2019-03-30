8911bebed2891b5c5829d527063c8c0a84b54f72
sumeet89 <sumeetrana@192.168.0.17>
2019-03-30 17:31:53 +0100

    Committing some stuff which allows testing the service on aws

--- yoda-web-services/src/main/java/com/yoda/webservice/controller/UserController.java
+++ yoda-web-services/src/main/java/com/yoda/webservice/controller/UserController.java
@@ -1,10 +1,18 @@
 package com.yoda.webservice.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+	
+	
+	@GetMapping
+	public String find() {
+		return "user";
+	}
+	
 
 }
--- yoda-web-services/src/main/java/com/yoda/webservice/repository/UserRepository.java
+++ yoda-web-services/src/main/java/com/yoda/webservice/repository/UserRepository.java
@@ -1,13 +1,7 @@
 package com.yoda.webservice.repository;
 
-import java.util.UUID;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-import com.yoda.webservice.entity.User;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, UUID> {
+public interface UserRepository  {
 
 }
